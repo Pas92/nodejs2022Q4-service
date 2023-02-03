cc2a8c059eab680a63e0c7d19f2ca14de9716c67
Pas92 <pavelburdovitsin92@gmail.com>
2023-02-03 14:47:15 +0300

    feat: create project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AlbumModule, TrackModule, ArtistModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
