bc7c4592e7fdb89f0d639762c7f7e50c3a161d1d
Pas92 <pavelburdavitsyn92@gmail.com>
2023-02-04 18:26:19 +0300

    feat: add dto validator for the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,18 @@ import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
+import { APP_PIPE } from '@nestjs/core';
+import { dtoValidationPipe } from './shared/validators/dto.validator';
 
 @Module({
   imports: [UserModule, AlbumModule, TrackModule, ArtistModule, FavsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: dtoValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
