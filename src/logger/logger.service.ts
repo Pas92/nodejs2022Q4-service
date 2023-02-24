be4582fa9bff22b196baaebcf914aed659875338
Pas92 <pavelburdovitsin92@gmail.com>
2023-02-24 17:04:21 +0300

    feat: add writing to file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, Logger, LogLevel, Inject } from '@nestjs/common';
 import 'dotenv/config';
 
+import { Stream } from 'stream';
+import { createWriteStream, existsSync, mkdirSync, WriteStream } from 'fs';
+
+type LogType = 'LOG' | 'ERROR' | 'WARN' | 'DEBUG' | 'VERBOSE';
+
 @Injectable()
 export class AppLogger extends Logger {
   private applyingLogLevel: LogLevel[];
+  private logfileStream: WriteStream;
 
   constructor(...args) {
     console.log('CREATE_LOGGER');
@@ -25,6 +31,18 @@ export class AppLogger extends Logger {
     }
     this.applyingLogLevel = avaliableLogLevels.slice(0, logLevel + 1);
 
+    if (!existsSync('./logs')) {
+      mkdirSync('./logs');
+    }
+    this.logfileStream = createWriteStream(
+      './logs/log' + new Date().toISOString() + '.log',
+      { flags: 'a' },
+    );
+
+    console.log(this.logfileStream.path);
+
+    this.logfileStream.write('hi');
+
     //TODO: Get the last log file
   }
 
@@ -35,8 +53,16 @@ export class AppLogger extends Logger {
    */
   log(message: any, ...optionalParams: any[]) {
     if (this.applyingLogLevel.includes('log')) {
+      Logger.attachBuffer();
       console.log('LOG_LOGGER');
       super.log(message, ...optionalParams);
+      console.log(Logger.logBuffer);
+      this.writeMessageToFile(
+        'LOG',
+        Logger.getTimestamp(),
+        Logger.logBuffer[0].arguments,
+      );
+      Logger.flush();
     }
   }
 
@@ -45,8 +71,17 @@ export class AppLogger extends Logger {
    */
   error(message: any, ...optionalParams: any[]) {
     if (this.applyingLogLevel.includes('error')) {
+      Logger.attachBuffer();
+
       console.log('ERROR_LOGGER');
       super.error(message, ...optionalParams);
+
+      this.writeMessageToFile(
+        'ERROR',
+        Logger.getTimestamp(),
+        Logger.logBuffer[0].arguments,
+      );
+      Logger.flush();
     }
   }
 
@@ -55,9 +90,18 @@ export class AppLogger extends Logger {
    */
   warn(message: any, ...optionalParams: any[]) {
     if (this.applyingLogLevel.includes('warn')) {
+      Logger.attachBuffer();
+
       console.log('WARN_LOGGER');
       super.warn(message, ...optionalParams);
     }
+
+    this.writeMessageToFile(
+      'WARN',
+      Logger.getTimestamp(),
+      Logger.logBuffer[0].arguments,
+    );
+    Logger.flush();
   }
 
   /**
@@ -65,9 +109,18 @@ export class AppLogger extends Logger {
    */
   debug(message: any, ...optionalParams: any[]) {
     if (this.applyingLogLevel.includes('debug')) {
+      Logger.attachBuffer();
+
       console.log('DEBUG_LOGGER');
       super.debug(message, ...optionalParams);
     }
+
+    this.writeMessageToFile(
+      'DEBUG',
+      Logger.getTimestamp(),
+      Logger.logBuffer[0].arguments,
+    );
+    Logger.flush();
   }
 
   /**
@@ -75,8 +128,30 @@ export class AppLogger extends Logger {
    */
   verbose(message: any, ...optionalParams: any[]) {
     if (this.applyingLogLevel.includes('verbose')) {
+      Logger.attachBuffer();
+
       console.log('VERBOSE_LOGGER');
       super.verbose(message, ...optionalParams);
+
+      this.writeMessageToFile(
+        'VERBOSE',
+        Logger.getTimestamp(),
+        Logger.logBuffer[0].arguments,
+      );
+      Logger.flush();
     }
+  }
+
+  private writeMessageToFile(
+    logType: LogType,
+    timestamp: string,
+    messages: unknown[],
+  ): void {
+    const context = messages.pop();
+
+    messages.forEach((message) => {
+      const log = `${timestamp} ${logType} [${context}] ${message}`;
+      console.log(log);
+    });
   }
 }
