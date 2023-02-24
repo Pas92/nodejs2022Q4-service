0733518b523d7d62b13a9a506b47ad5e4e7fcd5b
Pas92 <pavelburdovitsin92@gmail.com>
2023-02-24 17:00:39 +0300

    feat: add logging level to .env file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,16 +1,31 @@
-import {
-  Injectable,
-  Logger,
-  LoggerService,
-  ConsoleLogger,
-} from '@nestjs/common';
+import { Injectable, Logger, LogLevel, Inject } from '@nestjs/common';
+import 'dotenv/config';
 
 @Injectable()
 export class AppLogger extends Logger {
+  private applyingLogLevel: LogLevel[];
+
   constructor(...args) {
     console.log('CREATE_LOGGER');
-    //TODO: Get the last log file
     super(args[0], args[1]);
+
+    const avaliableLogLevels: LogLevel[] = [
+      'log',
+      'error',
+      'warn',
+      'debug',
+      'verbose',
+    ];
+
+    let logLevel: number = +process.env.LOG_LEVEL;
+    console.log(typeof logLevel, logLevel);
+
+    if (typeof logLevel !== 'number' || logLevel < 0 || logLevel > 4) {
+      logLevel = 2;
+    }
+    this.applyingLogLevel = avaliableLogLevels.slice(0, logLevel + 1);
+
+    //TODO: Get the last log file
   }
 
   //TODO: Add file logger
@@ -19,39 +34,49 @@ export class AppLogger extends Logger {
    * Write a 'log' level log.
    */
   log(message: any, ...optionalParams: any[]) {
-    console.log('LOG_LOGGER');
-    super.log(message, ...optionalParams);
+    if (this.applyingLogLevel.includes('log')) {
+      console.log('LOG_LOGGER');
+      super.log(message, ...optionalParams);
+    }
   }
 
   /**
    * Write an 'error' level log.
    */
   error(message: any, ...optionalParams: any[]) {
-    console.log('ERROR_LOGGER');
-    super.error(message, ...optionalParams);
+    if (this.applyingLogLevel.includes('error')) {
+      console.log('ERROR_LOGGER');
+      super.error(message, ...optionalParams);
+    }
   }
 
   /**
    * Write a 'warn' level log.
    */
   warn(message: any, ...optionalParams: any[]) {
-    console.log('WARN_LOGGER');
-    super.warn(message, ...optionalParams);
+    if (this.applyingLogLevel.includes('warn')) {
+      console.log('WARN_LOGGER');
+      super.warn(message, ...optionalParams);
+    }
   }
 
   /**
    * Write a 'debug' level log.
    */
   debug(message: any, ...optionalParams: any[]) {
-    console.log('DEBUG_LOGGER');
-    super.debug(message, ...optionalParams);
+    if (this.applyingLogLevel.includes('debug')) {
+      console.log('DEBUG_LOGGER');
+      super.debug(message, ...optionalParams);
+    }
   }
 
   /**
    * Write a 'verbose' level log.
    */
   verbose(message: any, ...optionalParams: any[]) {
-    console.log('VERBOSE_LOGGER');
-    super.verbose(message, ...optionalParams);
+    if (this.applyingLogLevel.includes('verbose')) {
+      console.log('VERBOSE_LOGGER');
+      super.verbose(message, ...optionalParams);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,22 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { LoggerFilter } from './logger/logger.filter';
-import { Logger } from '@nestjs/common';
+import { Logger, LogLevel } from '@nestjs/common';
 import { AppLogger } from './logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Exception filters
   const httpAdapter = app.get(HttpAdapterHost);
   const logger = app.get(AppLogger);
   app.useGlobalFilters(new LoggerFilter(httpAdapter, logger));
 
+  //Port
   const appConfig = app.get(ConfigService);
   const port = appConfig.get('PORT');
 
+  //Swagger
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Home Library service')
     .setDescription('Home Library service REST API by Pas92')
